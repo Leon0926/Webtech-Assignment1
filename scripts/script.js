6a97d47f105176483bf73375f9cbe22cdca62794
Leon's Laptop <102eon@gmail.com>
2022-12-05 01:30:51 -0800

    added button functions for cancel, new note

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,32 @@
-function clickHandler()
+function clickDarkTheme()
 {
     document.body.classList.toggle("darkMode")
 }
 
-darkTheme.addEventListener("click",clickHandler, {once:false});
+darkTheme.addEventListener("click",clickDarkTheme, {once:false});
+
+function clickCancel()
+{
+
+    document.getElementById("noteArea").style.display ='none';
+    document.getElementById("save").style.display ='none';
+    document.getElementById("cancel").style.display ='none';
+
+    
+}
+
+cancel.addEventListener("click",clickCancel, {once:false})
+
+function clickNewNote()
+{
+    document.getElementById("noteArea").style.display = 'block';
+    document.getElementById("save").style.display = 'block';
+    document.getElementById("cancel").style.display = 'block';
+
+    if(document.getElementById("noteArea").value !== '')
+    {
+        document.getElementById("noteArea").value = ''
+    }
+}
+
+newNote.addEventListener("click", clickNewNote,)
